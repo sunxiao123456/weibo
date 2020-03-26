2a5a2c63dab672d94d77896c0a4cafb464608ff8
sunxiao <529225469@qq.com>
2020-03-26 15:12:26 +0800

    增加注册逻辑

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -128,6 +128,17 @@ return [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
+    
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+    
+        'name' => [
+            'required' => '名称不能为空！',
+        ],
+        'password' => [
+            'required' => '密码不能为空！',
+        ]
     ],
 
     /*
